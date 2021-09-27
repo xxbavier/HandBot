629a9e62452912bb9bacfdc7f0f2d193d2714d03
xxbavier <43556706+xxbavier@users.noreply.github.com>
2021-09-27 01:45:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,6 @@ from itertools import cycle
 import sqlite3
 import math
 
-from os import environ
-from flask import Flask
-
-app = Flask(__name__)
-app.run(environ.get('PORT'))
-
 # Initiate
 
 intents = discord.Intents().all()
