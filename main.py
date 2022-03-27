2701232cc236460e675d20350cf5e78e57c30fbe
xxbavier <43556706+xxbavier@users.noreply.github.com>
2022-03-27 12:00:59 -0400

    Revert "enabled request ping"
    This reverts commit 9203ccce71f232bd941e9fa2dd77b69d2adc87ba.

--- main.py
+++ main.py
@@ -1199,7 +1199,7 @@ async def request(inter, gametime):
                         Button(label= "Claim", style= ButtonStyle.primary, custom_id= "Claim {}".format(job.label))
                     )
                 ],
-                content= "@everyone"
+                #content= "@everyone"
             )
 
             @int_bot.event
