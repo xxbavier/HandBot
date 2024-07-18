0335aeb3ecce5330279530e83df4a4b174a12533
xxbavier <xavrya10@gmail.com>
2024-07-18 12:15:56 -0400

    Added member join msg

--- main.py
+++ main.py
@@ -87,6 +87,32 @@ async def on_message(msg: discord.Message):
 
 @bot.event
 async def on_member_join(member: discord.Member):
+    embed = discord.Embed()
+    embed.title = "Welcome to *Handball: The League*."
+    embed.description = """Welcome to HTL, a league inspired by olympic handball!"""
+    embed.add_field(name="``What is Handball?``", value= """Handball is an olympic sport that is best described as a mixture of soccer and basketball.
+                    
+This server is a competitive league for a game on Roblox based on Olympic Handball--but with a few twists.""", inline= False)
+    embed.add_field(name="``Want to try the game out?``", value= """In HTL, there are 2 Roblox games that are most relevant to the league.
+                    
+There's [Handball 2 (HB2)]({}) and [Handball Association 1.16 (HBA 1.16)]({}).
+                    
+__HB2 is the game that is currently used for the league.__""".format(
+        "https://www.roblox.com/games/5498056786/Handball-2",
+        "https://www.roblox.com/games/7521555382/HBA-1-16"
+                    ), inline= False)
+    embed.add_field(name="``How do you join a team?``", value="""In HTL, Team Owners and General Managers tend to sign people they know.
+
+That being said, here's a few suggestions that can help get you signed to a team:
+1. Play pickups and get your name known by performing well.
+2. Make friends in HTL.
+3. Use <#1189285411974037574> to market your skills so that Team Coaches can see them.""", inline= False)
+    embed.add_field(name= "``Want to become a Team Owner?``", value= "You can become a Team Owner by filling in [this form](https://forms.gle/o56a5dyqYtSFu77m6).", inline=False)
+    embed.set_thumbnail(url=member.guild.icon)
+    embed.color = discord.Color.orange()
+
+    await member.send(content="https://discord.gg/z44MMBJ2Gr", embed= embed)
+
     roles = [
         1209253356829151272,
         1192374103609454683,
